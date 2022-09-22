b370386d7c34c1446bb4d56c3d3ff522f082d907
Andreas Rabenstein <andreas@rabenstein-page.de>
2022-09-22 23:44:38 +0200

    Fix path issues for favicon

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -6,6 +6,7 @@ from main import app
 html_content = open("dist/index.html").read().replace("\n", "")
 app.mount("/js", StaticFiles(directory="dist/js"), name="js")
 app.mount("/css", StaticFiles(directory="dist/css"), name="css")
+app.mount("/img", StaticFiles(directory="dist/img"), name="img")
 
 
 @app.get("/")
