e7b8bea2138f9871e39c9ed9392cdc0054737e07
Andreas Rabenstein <andreas@rabenstein-page.de>
2022-09-28 19:43:35 +0200

    Add env to build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,16 @@
+echo "=========== STARTING INSTALLATION =========== "
+echo "ENV"
+printenv
+echo "==="
+export VUE_APP_URL=wss://focused-meetings.azurewebsites.net/ws
 cd /frontend
 npm install
 npm run build
 mv dist /backend/app/dist
 cd /backend/app
+echo "ENV"
+printenv
+echo "==="
+echo "=========== INSTALLATION DONE =========== "
 
 uvicorn main:app --host 0.0.0.0 --port 80
